9833424c9ee96cbef67f7f2ce5b1f75a88a44be2
dravestus <dyyd.tk@gmail.com>
2018-10-20 14:07:18 +0300

    Changed to GET

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['HOST'] = os.environ['HOST']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # TODO: Look into it: https://stackoverflow.com/questions/33738467/how-do-i-know-if-i-can-disable-sqlalchemy-track-modifications
 db = SQLAlchemy(app)
 
@@ -16,7 +17,7 @@ def home():
   # TODO: Return index page with log of button presses
 
 
-@app.route('/button', methods=['POST'])
+@app.route('/button', methods=['GET'])
 def register_button_press():
   # TODO: User check maybe? Maybe allow users that are not present
   user_id = request.args['id']
@@ -30,7 +31,7 @@ def register_button_press():
 def view_log():
   # TODO: Add support for requesting specific part of log
   log_entries = ButtonPressLog.query.all()
-  return render_template('log.html', entries=log_entries)
+  return render_template('log.html', entries=reversed(log_entries))
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -43,12 +44,12 @@ def create_user():
 @app.route('/users')
 def list_users():
   users = User.query.all()
-  return render_template('users.html', users=users, host_url="https://ancient-crag-90092.herokuapp.com/")
+  return render_template('users.html', users=users, host_url=app.config['HOST'])
 
 @app.route('/users/test')
 def list_users_with_test():
   users = User.query.all()
-  return render_template('users.html', users=users, testing=True, host_url="https://ancient-crag-90092.herokuapp.com/")
+  return render_template('users.html', users=users, testing=True, host_url=app.config['HOST'])
 
 if __name__ == '__main__':
   app.run()
